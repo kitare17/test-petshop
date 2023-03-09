88a69336f542c24439d89900324db0e38beded2c
kitare17 <hoctapak@gmail.com>
2023-03-09 18:40:49 +0700

    tui thử nhánh

---
+++ src/java/model/service/UserService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.service;
+
+import model.entity.User;
+import model.repository.UserRepository;
+
+/**
+ *
+ * @author PC
+ */
+public class UserService {
+    public static User addUser(User user){
+        UserRepository.register(user);
+        return user;
+    }
+    public static boolean checkUserNameExist(String username){
+        return UserRepository.checkUserNameExist(username);
+    }
+}
